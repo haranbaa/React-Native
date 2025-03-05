eadd923ba800d059b374ce9caf7e91dbe7d61bb1
Abdullah <haranbaa97@gmail.com>
2025-03-06 00:43:22 +0100

    removed basket

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -4,41 +4,34 @@ import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "../screens/HomeScreen";
 import ShoeListScreen from "../screens/ShoeListScreen";
 import ShoeDetailsScreen from "../screens/ShoeDetailsScreen";
-import BasketScreen from "../screens/BasketScreen";
 import FavoritesScreen from "../screens/FavoritesScreen"; 
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
-  const [basket, setBasket] = useState([]); 
   const [favorites, setFavorites] = useState([]); 
 
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         {/* Home Screen */}
-        <Stack.Screen name="Home" options={{ title: "kyrie irving " }}>
+        <Stack.Screen name="Home" options={{ title: "Kyrie Irving" }}>
           {(props) => <HomeScreen {...props} />}
         </Stack.Screen>
 
         {/* Shoe List */}
         <Stack.Screen name="ShoeList" options={{ title: "NBA Shoes" }}>
-          {(props) => <ShoeListScreen {...props} basket={basket} setBasket={setBasket} favorites={favorites} setFavorites={setFavorites} />}
+          {(props) => <ShoeListScreen {...props} favorites={favorites} setFavorites={setFavorites} />}
         </Stack.Screen>
 
         {/* Shoe Details */}
         <Stack.Screen name="ShoeDetails" options={{ title: "Shoe Details" }}>
-          {(props) => <ShoeDetailsScreen {...props} basket={basket} setBasket={setBasket} favorites={favorites} setFavorites={setFavorites} />}
+          {(props) => <ShoeDetailsScreen {...props} favorites={favorites} setFavorites={setFavorites} />}
         </Stack.Screen>
 
         {/* Favorites */}
         <Stack.Screen name="Favorites" options={{ title: "Your Favorites" }}>
-          {(props) => <FavoritesScreen {...props} basket={basket} setBasket={setBasket} favorites={favorites} setFavorites={setFavorites} />}
-        </Stack.Screen>
-
-        {/* Basket */}
-        <Stack.Screen name="Basket" options={{ title: "Your Basket" }}>
-          {(props) => <BasketScreen {...props} basket={basket} setBasket={setBasket} />}
+          {(props) => <FavoritesScreen {...props} favorites={favorites} setFavorites={setFavorites} />}
         </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
